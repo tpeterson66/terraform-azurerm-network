70d8238c9f09d484f41589e0e750d036aa35b5a0
tpeterson66 <35910834+tpeterson66@users.noreply.github.com>
2022-10-06 17:34:54 -0400

    inital commit for vnet and subnet

---
+++ modules/subnet/main.tf
@@ -0,0 +1,74 @@
+variable "name" {
+    type = string
+    description = "The name of the subnet"
+}
+
+variable "resource_group_name" {
+    type = string
+    description = "The resource group for the resource"
+}
+
+variable "virtual_network_name" {
+    type = string
+    description "Name of the virtual network"
+}
+
+variable "address_prefixes" {
+    type = list(string)
+    description = "The subnet address prefix"
+}
+
+variable "service_endpoints" {
+    type = list(string)
+    description = "list of service endpoints"
+}
+
+variable "enforce_private_link_endpoint_network_policies" {
+    type = bool
+    description = "enforce private link policies"
+}
+
+variable "enforce_private_link_service_network_policies" {
+    type = bool
+    description = "enforce network policies"
+}
+
+variable "settings" {
+    default = {
+        delegation = {
+            name = "delegation"
+            service_delegation = "Microsoft.ContainerInstance/containerGroups"
+            actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+        }
+    }
+}
+
+
+resource "azurerm_subnet" "subnet" {
+
+  name                                           = var.name
+  resource_group_name                            = var.resource_group_name
+  virtual_network_name                           = var.virtual_network_name
+  address_prefixes                               = var.address_prefixes
+  service_endpoints                              = var.service_endpoints
+  enforce_private_link_endpoint_network_policies = try(var.enforce_private_link_endpoint_network_policies, false)
+  enforce_private_link_service_network_policies  = try(var.enforce_private_link_service_network_policies, false)
+
+  dynamic "delegation" {
+    for_each = try(var.settings.delegation, null) == null ? [] : [1]
+
+    content {
+      name = var.settings.delegation.name
+
+      service_delegation {
+        name    = var.settings.delegation.service_delegation
+        actions = lookup(var.settings.delegation, "actions", null)
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [name]
+  }
+
+}
---
+++ modules/virtual_network/main.tf
@@ -0,0 +1,54 @@
+# variables
+variable "name" {
+    type = string
+    description = "The name of the virtual network"
+}
+
+variable "location" {
+    type = string
+    description = "The Azure Region where the virutal network will be deployed"
+}
+variable "resource_group_name" {
+    type = string
+    description = "The Azure Resource Group Name for management"
+}
+variable "address_space" {
+    type = list(string)
+    description = "The address space for the virtaul network"
+}
+variable "tags" {
+  type        = map(string)
+  description = "A map of the tags to use on the resources that are deployed with this module."
+
+  default = {
+    source = "terraform"
+  }
+}
+
+resource "azurerm_virtual_network" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.address_space
+  tags                = var.tags
+
+  dns_servers = flatten(
+    concat(
+      try(lookup(var.settings.vnet, "dns_servers", [])),
+      try(local.dns_servers_process, [])
+    )
+  )
+
+  dynamic "ddos_protection_plan" {
+    for_each = var.ddos_id != "" || can(var.global_settings["ddos_protection_plan_id"]) ? [1] : []
+
+    content {
+      id     = var.ddos_id != "" ? var.ddos_id : var.global_settings["ddos_protection_plan_id"]
+      enable = true
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [name]
+  }
+}
