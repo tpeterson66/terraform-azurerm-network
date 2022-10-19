aeac484d552608ad13780761ce28260896a1b118
Ahmed Babic <babicahmed@outlook.com>
2022-10-19 19:57:40 +0200

    fixed nsg to dynamic sec rule

--- modules/network_security_group/main.tf
+++ modules/network_security_group/main.tf
@@ -27,41 +27,20 @@ variable "resource_group_name" {
   description = "The name of the resource group in which to create the network security group."
   type        = string
 }
-variable "security_rule_name" {
-  description = "The name of the security rule that is used in network security group"
-  type        = string
-}
-variable "security_rule_priority" {
-  description = "Specifies the priority of the rule. The value can be between 100 and 4096 and must be unique for each rule in the collection. (Lower priority number -> Higher priority)."
-  type        = number
-}
-variable "security_rule_direction" {
-  description = "The direction specifies if rule will be evaluated on incoming or outgoing traffic. Possible values are Inbound and Outbound."
-  type        = string
-}
-variable "security_rule_access" {
-  description = "Specifies whether network traffic is allowed or denied. Possible values are Allow and Deny."
-  type        = string
-}
-variable "security_rule_protocol" {
-  description = "Network protocol this rule applies to. Possible values include Tcp, Udp, Icmp, Esp, Ah or * (which matches all)."
-  type        = string
-}
-variable "security_rule_source_port_range" {
-  description = "Source Port or Range. Integer or range between 0 and 65535 or * to match any. This is required if source_port_ranges is not specified."
-  type        = string
-}
-variable "security_rule_destination_port_range" {
-  description = "Destination Port or Range. Integer or range between 0 and 65535 or * to match any. This is required if destination_port_ranges is not specified."
-  type        = string
-}
-variable "security_rule_source_address_prefix" {
-  description = "CIDR or source IP range or * to match any IP. Tags such as ‘VirtualNetwork’, ‘AzureLoadBalancer’ and ‘Internet’ can also be used. This is required if source_address_prefixes is not specified."
-  type        = string
-}
-variable "security_rule_destination_address_prefix" {
-  description = "CIDR or destination IP range or * to match any IP. Tags such as ‘VirtualNetwork’, ‘AzureLoadBalancer’ and ‘Internet’ can also be used. Besides, it also supports all available Service Tags like ‘Sql.WestEurope‘, ‘Storage.EastUS‘, etc. You can list the available service tags with the CLI: shell az network list-service-tags --location westcentralus"
-  type        = string
+variable "security_rules" {
+  type        = list(object({
+    name                       = string # The name for the security rule
+    priority                   = number # Specifies the priority of the rule. The value can be between 100 and 4096 and must be unique for each rule in the collection. (Lower priority number -> Higher priority).
+    direction                  = string # The direction specifies if rule will be evaluated on incoming or outgoing traffic. Possible values are Inbound and Outbound.
+    access                     = string # Specifies whether network traffic is allowed or denied. Possible values are Allow and Deny.
+    protocol                   = string # Network protocol this rule applies to. Possible values include Tcp, Udp, Icmp, Esp, Ah or * (which matches all).
+    source_port_range          = string # Source Port or Range. Integer or range between 0 and 65535 or * to match any. This is required if source_port_ranges is not specified.
+    destination_port_range     = string # Destination Port or Range. Integer or range between 0 and 65535 or * to match any. This is required if destination_port_ranges is not specified.
+    source_address_prefix      = string # CIDR or source IP range or * to match any IP. Tags such as ‘VirtualNetwork’, ‘AzureLoadBalancer’ and ‘Internet’ can also be used. This is required if source_address_prefixes is not specified.
+    destination_address_prefix = string # CIDR or destination IP range or * to match any IP. Tags such as ‘VirtualNetwork’, ‘AzureLoadBalancer’ and ‘Internet’ can also be used. Besides, it also supports all available Service Tags like ‘Sql.WestEurope‘, ‘Storage.EastUS‘, etc. You can list the available service tags with the CLI: shell az network list-service-tags --location westcentralus
+  }))
+  description = "List of security rules to be added to network security group."
+  default     = []
 }
 
 # resources
@@ -74,21 +53,23 @@ resource "azurerm_network_security_group" "this" {
   tags                = var.tags
 # network security rule
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_security_rule
-  security_rule {
-    name                       = var.security_rule_name
-    priority                   = var.security_rule_priority
-    direction                  = var.security_rule_direction
-    access                     = var.security_rule_access
-    protocol                   = var.security_rule_protocol
-    source_port_range          = var.security_rule_source_port_range
-    destination_port_range     = var.security_rule_destination_port_range
-    source_address_prefix      = var.security_rule_source_address_prefix
-    destination_address_prefix = var.security_rule_destination_address_prefix
+  dynamic "security_rule" {
+    for_each = var.security_rules
+    content {
+      name                       = security_rule.value["name"]
+      priority                   = security_rule.value["priority"]
+      direction                  = security_rule.value["direction"]
+      access                     = security_rule.value["access"]
+      protocol                   = security_rule.value["protocol"]
+      source_port_range          = security_rule.value["source_port_range"]
+      destination_port_range     = security_rule.value["destination_port_range"]
+      source_address_prefix      = security_rule.value["source_address_prefix"]
+      destination_address_prefix = security_rule.value["destination_address_prefix"]
+    }
   }
 }
 
 # outputs
 output "id" {
-  value       = azurerm_network_security_group.this.id
-  description = "Network Security Group ID"
+  value = azurerm_network_security_group.this.id
 }
