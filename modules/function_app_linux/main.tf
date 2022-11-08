4fbb5c857fa6b80bcd4d9194b396f6c7324c4f86
Ahmed Babic <babicahmed@outlook.com>
2022-11-08 16:28:55 +0100

    diagnotic setting and metric alert with function

--- modules/function_app_linux/main.tf
+++ modules/function_app_linux/main.tf
@@ -43,6 +43,36 @@ variable "elastic_instance_minimum" {
   type        = number
   description = "The number of minimum instances for this function app. Only affects apps on the Premium plan."
 }
+variable "enable_diagnostics"{
+  type = bool
+  description = "Do you want to enable monitor diagnostic setting for this resource?"
+}
+variable "log_analytics_workspace_id" {
+  description = "The name of the resource group in which to create the Storage account."
+  type        = string
+}
+variable "enable_metric_alerts"{
+  description = "Do you want to enable monitor metric alert for this resource?"
+  type        = bool
+}
+variable "metric_alerts"{
+  description = "Monitor Metric alert variables to be passed"
+  type        = list(object({
+      name               = string
+      metric_namespace   = string
+      metric_name        = string
+      aggregation        = string
+      operator           = string
+      threshold          = string
+      dimension_name     = string
+      dimension_operator = string
+      dimension_values   = list(string)
+    }))
+}
+variable "action_group_id"{
+  description = "The ID of action group to be used in monitoring alerts."
+  type = string
+}
 
 # resources
 # linux function app
@@ -61,6 +91,28 @@ resource "azurerm_linux_function_app" "this" {
   lifecycle {
     ignore_changes = [site_config, app_settings, tags]
   }
+}
+
+# modules
+# monitor diagnostic settings
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/monitor_diagnostic_setting
+module "monitor_diagnostic_setting" {
+  source = "../monitor_diagnostic_settings"
+
+  name                       = "${var.name}_diagnostic_setting"
+  target_resource_id         = azurerm_linux_function_app.this.id
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+  enable_diagnostics         = var.enable_diagnostics
+}
+# metric alert
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/monitor_metric_alert
+module "monitor_metric_alerts" {
+  source = "../monitor_metric_alert"
+  resource_group_name  = var.resource_group_name
+  scopes               = [azurerm_linux_function_app.this.id]
+  metric_alerts        = var.metric_alerts
+  action_group_id      = var.action_group_id
+  enable_metric_alerts = var.enable_metric_alerts
 }
 
 # outputs
