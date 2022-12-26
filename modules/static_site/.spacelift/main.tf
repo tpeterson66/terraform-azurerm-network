075788ddb62a2ab77b42574f112c9a77f9070910
Ahmed Babic <babicahmed@outlook.com>
2022-12-26 12:59:24 +0100

    spacelift static site test

---
+++ modules/static_site/.spacelift/main.tf
@@ -0,0 +1,27 @@
+provider "azurerm" {
+  features {}
+}
+
+// resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "spacelift-test-static"
+  location = "EastUS2"
+}
+
+// Static site
+module "linux_web_app" {
+  source              = "../"
+  name                = "spacelift-static-site"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags = {
+    environment = "module-testing"
+    retention   = "DeleteAtWill"
+    owner       = "spacelift"
+  }
+  sku_tier = "Standard"
+  sku_size = "Standard"
+  identity_type = "UserAssigned"
+  user_assigned_identities = ["/subscriptions/d473e918-7273-4745-9214-3f7b999863a3/resourceGroups/spacelift-test-static-site/providers/Microsoft.ManagedIdentity/userAssignedIdentities/spacelift-test-user-assigned-managed-identity"]
+}  
+
