5e61463e0f5eb208cdf8dea32d7fdd5c9f297dcb
Ahmed Babic <babicahmed@outlook.com>
2022-12-29 12:32:50 +0100

    function app linux with metrics

--- modules/function_app_linux/.spacelift/main.tf
+++ modules/function_app_linux/.spacelift/main.tf
@@ -9,8 +9,8 @@ module "function_app_linux" {
   location                     = "EastUS"
   resource_group_name          = "spacelift-test-modules"
   service_plan_id              = "/subscriptions/d473e918-7273-4745-9214-3f7b999863a3/resourceGroups/spacelift-test-modules/providers/Microsoft.Web/serverfarms/spacelift-test-serviceplanlinux"
-  storage_account_name         = "ahmedbstorageaccount"
-  storage_account_access_key   = "Np7Qr+WFcrKm9eCrL+6DwGb3aYxZzAS87V5UjK9e/ej9MkrFWz1RedXh0F2E7CLBRC9nYRXrzinu+AStSNUEfg=="
+  storage_account_name         = "storageaccountname"
+  storage_account_access_key   = "storagekey"
   elastic_instance_minimum     = "2"
   enable_diagnostics           = false
   log_analytics_workspace_id   = ""
@@ -32,8 +32,8 @@ module "function_app_linux_diagnostic" {
   location                     = "EastUS"
   resource_group_name          = "spacelift-test-modules"
   service_plan_id              = "/subscriptions/d473e918-7273-4745-9214-3f7b999863a3/resourceGroups/spacelift-test-modules/providers/Microsoft.Web/serverfarms/spacelift-test-serviceplanlinux"
-  storage_account_name         = "ahmedbstorageaccount"
-  storage_account_access_key   = "Np7Qr+WFcrKm9eCrL+6DwGb3aYxZzAS87V5UjK9e/ej9MkrFWz1RedXh0F2E7CLBRC9nYRXrzinu+AStSNUEfg=="
+  storage_account_name         = "storageaccountname"
+  storage_account_access_key   = "storagekey"
   elastic_instance_minimum     = "2"
   enable_diagnostics           = true
   log_analytics_workspace_id   = "/subscriptions/d473e918-7273-4745-9214-3f7b999863a3/resourceGroups/spacelift-test-modules/providers/Microsoft.OperationalInsights/workspaces/spacelift-test-loganalyticsworkspace"
@@ -48,21 +48,21 @@ module "function_app_linux_diagnostic" {
   }
 }
 
-# // function app linux with diagnostic settings
-module "function_app_linux_diagnostic" {
+# // function app linux with diagnostic settings and metric alerts
+module "function_app_linux_diagnostic_metric" {
   source                       = "../"
-  name                         = "spacelift-function-app-linux-diagnostic"
+  name                         = "spacelift-function-app-linux-diagnostic-metricalert"
   location                     = "EastUS"
   resource_group_name          = "spacelift-test-modules"
   service_plan_id              = "/subscriptions/d473e918-7273-4745-9214-3f7b999863a3/resourceGroups/spacelift-test-modules/providers/Microsoft.Web/serverfarms/spacelift-test-serviceplanlinux"
-  storage_account_name         = "ahmedbstorageaccount"
-  storage_account_access_key   = "Np7Qr+WFcrKm9eCrL+6DwGb3aYxZzAS87V5UjK9e/ej9MkrFWz1RedXh0F2E7CLBRC9nYRXrzinu+AStSNUEfg=="
+  storage_account_name         = "storageaccountname"
+  storage_account_access_key   = "storagekey"
   elastic_instance_minimum     = "2"
   enable_diagnostics           = true
   log_analytics_workspace_id   = "/subscriptions/d473e918-7273-4745-9214-3f7b999863a3/resourceGroups/spacelift-test-modules/providers/Microsoft.OperationalInsights/workspaces/spacelift-test-loganalyticsworkspace"
-  enable_metric_alerts         = false
-  metric_alerts                = [  ]
-  action_group_id              = ""
+  enable_metric_alerts         = true
+  metric_alerts                = [{name = "spacelift-test-metricalert", metric_namespace = "Microsoft.Web/Sites", metric_name = "Requests", aggregation = "Total", operator = "GreaterThan", threshold = "100", dimension_name = "Instance", dimension_operator = "Include", dimension_values = ["*"] }]
+  action_group_id              = "/subscriptions/D473E918-7273-4745-9214-3F7B999863A3/resourceGroups/spacelift-test-modules/providers/microsoft.insights/actionGroups/spacelift-test-actiongroup"
 
   tags = {
     environment = "module-testing"
@@ -70,3 +70,4 @@ module "function_app_linux_diagnostic" {
     owner       = "spacelift"
   }
 }
+
