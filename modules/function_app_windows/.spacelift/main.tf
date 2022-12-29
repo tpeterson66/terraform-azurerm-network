ee66c883bde4c117943a44b176ceef56fe4f20a0
Ahmed Babic <babicahmed@outlook.com>
2022-12-29 11:58:10 +0100

    function app windows

--- modules/function_app_windows/.spacelift/main.tf
+++ modules/function_app_windows/.spacelift/main.tf
@@ -9,8 +9,8 @@ module "function_app_windows" {
   location                     = "EastUS"
   resource_group_name          = "spacelift-test-modules"
   service_plan_id              = "/subscriptions/d473e918-7273-4745-9214-3f7b999863a3/resourceGroups/spacelift-test-modules/providers/Microsoft.Web/serverfarms/spacelift-test-serviceplanwindows"
-  storage_account_name         = "ahmedbstorageaccount"
-  storage_account_access_key   = "Np7Qr+WFcrKm9eCrL+6DwGb3aYxZzAS87V5UjK9e/ej9MkrFWz1RedXh0F2E7CLBRC9nYRXrzinu+AStSNUEfg=="
+  storage_account_name         = "storageaccountname"
+  storage_account_access_key   = "keyvalue"
   elastic_instance_minimum     = "2"
 
   tags = {
