09a3f8c87a0bf4c09afcc032cdb592eb51301602
tpeterson66 <35910834+tpeterson66@users.noreply.github.com>
2023-01-28 15:52:12 -0500

    Tpeterson/spacelift config (#40)
    * updates to management
    
    * updating naming of spaces

--- management/azure-team.tf
+++ management/azure-team.tf
@@ -1,6 +1,6 @@
 # azure team - used to manage the lab environments
 resource "spacelift_space" "azure_team" {
-  name             = "azure-team"
+  name             = "smartcloud-demo"
   parent_space_id  = spacelift_space.coretek.id
   inherit_entities = true
 }
--- management/managed-services.tf
+++ management/managed-services.tf
@@ -1,6 +1,6 @@
 # managed-services - used to manage customer environemnts
 resource "spacelift_space" "managed_services" {
-  name             = "azure-team"
+  name             = "azure-managed-services"
   parent_space_id  = spacelift_space.coretek.id
   inherit_entities = true
 }
