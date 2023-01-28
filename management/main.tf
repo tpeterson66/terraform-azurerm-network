13578c49b2d9dc70ab64782316da8401c4c82ecb
tpeterson66 <35910834+tpeterson66@users.noreply.github.com>
2023-01-28 20:46:37 +0000

    updates to management

--- management/main.tf
+++ management/main.tf
@@ -11,37 +11,13 @@ provider "spacelift" {
   # Configuration options
 }
 
+# root space
+data "spacelift_space" "root" {
+  space_id = "root"
+}
+
 # Coretk Space
 resource "spacelift_space" "coretek" {
   name             = "coretek"
   inherit_entities = true
-}
-
-# Module Management Space
-resource "spacelift_space" "modules" {
-  name             = "coretek-modules"
-  parent_space_id  = spacelift_space.coretek.id
-  inherit_entities = true
-}
-
-module "route_table" {
-  source   = "./modules/module"
-  name     = "terraform-azurerm-route_table"
-  space_id = spacelift_space.modules.id
-  branch       = "main"
-  description  = "Azure Route Table"
-  repository   = "terraform-azurerm-network"
-  project_root = "modules/route_table"
-  namespace    = "tpeterson66"
-}
-
-module "static_site" {
-  source         = "./modules/module"
-  name           = "terraform-azurerm-static_site"
-  space_id       = spacelift_space.modules.id
-  branch         = "main"
-  description    = "Azure Static Site"
-  repository     = "terraform-azurerm-network"
-  project_root   = "modules/static_site"
-  namespace      = "tpeterson66"
 }
