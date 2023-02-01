c3ccb8b8142d874a7219a56e2d834b95b7e4b566
Tom Peterson <tpeterson@Toms-Mac-mini.local>
2023-02-01 12:58:05 -0500

    updates to add additional files

--- management/azure-team.tf
+++ management/azure-team.tf
@@ -28,3 +28,26 @@ resource "spacelift_azure_integration_attachment" "smartcloud_dev" {
   read            = true
   subscription_id = "c759eb32-e9c8-4e19-9f2f-d036cf250f5c"
 }
+
+## Azure Designer
+resource "spacelift_stack" "azure_designer" {
+  github_enterprise {
+    namespace = "tpeterson66" # The GitHub organization / user the repository belongs to
+  }
+  administrative    = false
+  autodeploy        = true
+  branch            = "main"
+  description       = "Smartcloud dev management"
+  name              = "Azure Designer"
+  project_root      = "smartcloud/azureDesigner"
+  repository        = "terraform-azurerm-network"
+  terraform_version = "1.3.7"
+}
+
+resource "spacelift_azure_integration_attachment" "azure_designer" {
+  integration_id  = data.spacelift_azure_integration.azure_designer.id
+  stack_id        = spacelift_stack.azure_designer.id
+  write           = true
+  read            = true
+  subscription_id = "c759eb32-e9c8-4e19-9f2f-d036cf250f5c"
+}
