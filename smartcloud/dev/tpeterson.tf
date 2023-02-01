afd976262fd14d070f2f93776d0d284fee9b08f0
tpeterson66 <35910834+tpeterson66@users.noreply.github.com>
2023-01-31 20:17:25 -0500

    Tpeterson/smartcloud (#45)
    * updates to support azure team
    
    * updates to support azure team
    
    * updates to test new resource group
    
    ---------
    
    Co-authored-by: Tom Peterson <tpeterson@M-ALNVTP-1619.home.tpeterson.io>

---
+++ smartcloud/dev/tpeterson.tf
@@ -0,0 +1,13 @@
+resource "azurerm_resource_group" "tpeterson_rg" {
+  name     = "tpeterson-azure-designer"
+  location = "East US"
+  tags = {
+    owner = "Tom Peterson"
+  }
+}
+
+resource "azurerm_role_assignment" "tpeterson_rg_role" {
+  scope                = azurerm_resource_group.tpeterson_rg.id
+  role_definition_name = "Owner"
+  principal_id         = var.users.tpeterson
+}
