e3799a3b3afb58f7d9728f307a761061622f4204
Tom Peterson <tpeterson@M-ALNVTP-1619.local>
2023-02-02 18:23:04 -0500

    updates to module and updates to create new stack

--- modules/virtual_network/.spacelift/main.tf
+++ modules/virtual_network/.spacelift/main.tf
@@ -15,7 +15,7 @@ module "virtual_network" {
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
   ddos_id             = "/subscriptions/d473e918-7273-4745-9214-3f7b999863a3/resourceGroups/spacelift-test-modules/providers/Microsoft.Network/ddosProtectionPlans/spacelift-ddos"
-  
+
   tags = {
     environment = "module-testing"
     retention   = "DeleteAtWill"
@@ -31,7 +31,7 @@ module "virtual_network_dns" {
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
   ddos_id             = "/subscriptions/d473e918-7273-4745-9214-3f7b999863a3/resourceGroups/spacelift-test-modules/providers/Microsoft.Network/ddosProtectionPlans/spacelift-ddos"
-  
+
   settings = [{
     dns_servers = ["10.0.0.5", "10.0.0.6"]
   }]
