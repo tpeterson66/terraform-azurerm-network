bec92d5af78646193193410bec925dbcaed7429e
Ahmed Babic <babicahmed@outlook.com>
2023-02-03 15:55:35 +0100

    name fix

--- modules/load_balancer/.spacelift/main.tf
+++ modules/load_balancer/.spacelift/main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
 }
 resource "azurerm_resource_group" "rg" {
-  name     = "spacelift-test-serviceplan"
+  name     = "spacelift-test-lb"
   location = "EastUS"
 }
 
