e89e4ea69aa8a41adbe158d2ca5f1d108c03e07a
tpeterson66 <35910834+tpeterson66@users.noreply.github.com>
2023-02-02 20:27:18 -0500

    updates to support private worker (#61)
    * updates to support private worker
    
    * updates to remove ddos
    
    * removing ddos until we can fix later
    
    * adding worker pool to modules
    
    * updates to module module
    
    * addind data resource back in
    
    ---------
    
    Co-authored-by: Tom Peterson <tpeterson@M-ALNVTP-1619.local>

--- modules/virtual_network/.spacelift/main.tf
+++ modules/virtual_network/.spacelift/main.tf
@@ -14,7 +14,7 @@ module "virtual_network" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
-  ddos_id             = "/subscriptions/d473e918-7273-4745-9214-3f7b999863a3/resourceGroups/spacelift-test-modules/providers/Microsoft.Network/ddosProtectionPlans/spacelift-ddos"
+  # ddos_id             = "/subscriptions/d473e918-7273-4745-9214-3f7b999863a3/resourceGroups/spacelift-test-modules/providers/Microsoft.Network/ddosProtectionPlans/spacelift-ddos"
 
   tags = {
     environment = "module-testing"
@@ -30,7 +30,7 @@ module "virtual_network_dns" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
-  ddos_id             = "/subscriptions/d473e918-7273-4745-9214-3f7b999863a3/resourceGroups/spacelift-test-modules/providers/Microsoft.Network/ddosProtectionPlans/spacelift-ddos"
+  # ddos_id             = "/subscriptions/d473e918-7273-4745-9214-3f7b999863a3/resourceGroups/spacelift-test-modules/providers/Microsoft.Network/ddosProtectionPlans/spacelift-ddos"
 
   settings = [{
     dns_servers = ["10.0.0.5", "10.0.0.6"]
