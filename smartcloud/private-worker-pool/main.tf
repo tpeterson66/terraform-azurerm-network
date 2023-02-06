f79eef08e2905fd90441d7b42945fe72c7912323
Tom Peterson <tpeterson@Toms-Mac-mini.local>
2023-02-06 10:03:21 -0500

    updates to cover new deployment of spacelift VM

--- smartcloud/private-worker-pool/main.tf
+++ smartcloud/private-worker-pool/main.tf
@@ -24,3 +24,83 @@ module "terraform-azurerm-virtual_network" {
   # settings = list(object({  dns_servers = list(string)  }))
 }
 
+resource "azurerm_subnet" "worker_pool" {
+  name                 = "WorkerPool"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = module.terraform-azurerm-virtual_network.name
+  address_prefixes     = ["10.224.0.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "vm-spaceliftworkerpool"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = azurerm_resource_group.rg.tags
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.worker_pool.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "worker_pool" {
+  name                = "spaceliftworkerpool"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  tags                = azurerm_resource_group.rg.tags
+  size                = "Standard_F2"
+  admin_username      = "coretekadmin"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "coretekadmin"
+    public_key = file("./id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_public_ip" "worker_pool" {
+  name                = "vm-spaceliftworkerpool"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  tags                = azurerm_resource_group.rg.tags
+}
+
+resource "azurerm_network_security_group" "worker_pool" {
+  name                = "vm-spaceliftworkerpool"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = azurerm_resource_group.rg.tags
+
+  security_rule {
+    name                       = "tpeterson_home"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "162.197.55.204/32"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "worker_pool" {
+  subnet_id                 = azurerm_subnet.worker_pool
+  network_security_group_id = azurerm_network_security_group.worker_pool.id
+}
