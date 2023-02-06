8680824324653348ea0bda0ab58b9ae0d950d318
babicahmed <115175932+babicahmed@users.noreply.github.com>
2023-02-06 15:04:22 +0100

    Fix for spacelift module : route (#62)
    * Fix for spacelift module : route
    
    * added fmt
    
    * route table dependency

--- modules/route/.spacelift/main.tf
+++ modules/route/.spacelift/main.tf
@@ -6,17 +6,12 @@ resource "azurerm_resource_group" "rg" {
   name     = "spacelift-test-lb"
   location = "EastUS"
 }
-
-# // route
-module "routes" {
-  source                 = "../"
-  name                   = "spacelift-testroute"
-  location               = azurerm_resource_group.rg.location
-  resource_group_name    = azurerm_resource_group.rg.name
-  route_table_name       = "spacelift-testroute"
-  address_prefix         = "10.1.0.0/16"
-  next_hop_type          = "VnetLocal"
-  next_hop_in_ip_address = null
+# // route table
+module "route_table" {
+  source              = "./modules/route_table/"
+  name                = "spacelift-test-standard-route-table"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   tags = {
     environment = "module-testing"
     retention   = "DeleteAtWill"
@@ -24,19 +19,25 @@ module "routes" {
   }
 }
 
+# // route
+module "routes" {
+  source                 = "../"
+  name                   = "spacelift-testroute"
+  resource_group_name    = azurerm_resource_group.rg.name
+  route_table_name       = module.route_table.name
+  address_prefix         = "10.1.0.0/16"
+  next_hop_type          = "VnetLocal"
+  next_hop_in_ip_address = null
+}
+
 # // route virtual appliance
 module "routes_virtualappliance" {
   source                 = "../"
   name                   = "spacelift-testroute1"
-  location               = azurerm_resource_group.rg.location
   resource_group_name    = azurerm_resource_group.rg.name
-  route_table_name       = "spacelift-testroute"
+  route_table_name       = module.route_table.name
   address_prefix         = "4.5.6.7/32"
   next_hop_type          = "VirtualAppliance"
   next_hop_in_ip_address = "10.20.30.40"
-  tags = {
-    environment = "module-testing"
-    retention   = "DeleteAtWill"
-    owner       = "spacelift"
-  }
+
 }
